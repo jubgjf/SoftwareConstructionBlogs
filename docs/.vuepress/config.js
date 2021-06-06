bfd8f4fb3fe5f2df7b235f1340e1e009ec6bcd22
jubgjf <guanjiannan@outlook.com>
2021-06-06 10:45:15 +0800

    enable codeblock line numbers

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,9 @@ module.exports = {
             delimiters: 'dollars'
         }
     },
+    markdown: {
+        lineNumbers: true
+    },
     theme: 'default-prefers-color-scheme',
     themeConfig: {
         nav: [
